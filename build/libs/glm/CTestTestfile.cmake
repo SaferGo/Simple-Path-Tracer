8aa15275ffe708072b87a472964bcaefb73b4f50
safergo <sfgomez1998@gmail.com>
2022-07-12 19:29:21 +0200

    removed ignored directory

--- build/libs/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/code/graphics/Simple-Ray-Tracer/libs/glm
-# Build directory: /home/santiago/code/graphics/Simple-Ray-Tracer/build/libs/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/libs/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/code/graphics/Simple-Ray-Tracer/libs/glm/glm
-# Build directory: /home/santiago/code/graphics/Simple-Ray-Tracer/build/libs/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
